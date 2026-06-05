ca09d486b0af5b1abf0b1a5da5d655d519be42f8
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2026-06-05 23:05:31 +0600

    d

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -352,7 +352,7 @@ const buyProduct = async (
   }
 
   // price + commission
-  const totalAmount = buyProduct.price + buyProduct.commission
+  const totalAmount =  buyProduct.commission + buyProduct.price;
 
   // Update user balance
   const updatedUser = await User_Model.findByIdAndUpdate(
